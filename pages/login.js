c38a4296af0432de7806357bdb5cf66e156c3c80
Barneslow <darrachb1991@gmail.com>
2022-10-28 19:35:27 +0100

    updated background

--- pages/login.js
+++ pages/login.js
@@ -35,45 +35,44 @@ const Login = () => {
 
   const handleLoginWithEmail = async (e) => {
     e.preventDefault();
-    setIsLoading(true);
 
     if (email) {
-      if (email === "darrachb1991@gmail.com") {
-        try {
-          const didToken = await magic.auth.loginWithMagicLink({
-            email,
+      // log in a user by their email
+      try {
+        setIsLoading(true);
+
+        const didToken = await magic.auth.loginWithMagicLink({
+          email,
+        });
+        if (didToken) {
+          const response = await fetch("/api/login", {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${didToken}`,
+              "Content-Type": "application/json",
+            },
           });
 
-          if (didToken) {
-            const response = await fetch("/api/login", {
-              method: "POST",
-              headers: {
-                Authorization: `Bearer ${didToken}`,
-                "Content-Type": "application/json",
-              },
-            });
-
-            const loggedInResponse = await response.json();
-            if (loggedInResponse.done) {
-              router.push("/");
-            } else {
-              setIsLoading(false);
-              setUserMsg("Something went wrong logging in");
-            }
+          const loggedInResponse = await response.json();
+          if (loggedInResponse.done) {
+            router.push("/");
+          } else {
+            setIsLoading(false);
+            setUserMsg("Something went wrong logging in");
           }
-        } catch (error) {
-          setIsLoading(false);
-          console.error("Something went wrong logging in", error);
         }
-      } else {
+      } catch (error) {
+        // Handle errors if required!
+        console.error("Something went wrong logging in", error);
         setIsLoading(false);
-        setUserMsg("Something went wrong logging in");
       }
     } else {
+      // show user message
       setIsLoading(false);
       setUserMsg("Enter a valid email address");
     }
   };
+
   return (
     <div className={styles.container}>
       <Head>
